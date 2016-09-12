847569e2dbb80974b4670af457e55f788b256661
otolithic <otolithic@users.noreply.github.com>
2016-09-12 00:53:30 -0500

    Update euler9.m

--- euler9.m
+++ euler9.m
@@ -1,11 +1,14 @@
+%find the pythagoream triplet for which a + b + c = 1000
+
+%NOTE: THIS IS UNFINISHED
+
 a = 1:1000;
 b = 1:1000;
 c = 1:1000;
 
-[A, B] = meshgrid(a, b);
-
-X = A + B + sqrt(A.^2 + B.^2) - 1000);
-
-disp(X[1000 1000]);
+% ?????
+%[A, B] = meshgrid(a, b);
+%X = A + B + sqrt(A.^2 + B.^2) - 1000);
+%disp(X[1000 1000]);
 
 
